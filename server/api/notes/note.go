ff76c4e1a5ca9000c11a0cf0573842bb63f16ff1
David Lu <david.lu@keeptruckin.com>
2020-08-31 17:07:07 -0400

    WIP adding endpoint

--- server/api/notes/note.go
+++ server/api/notes/note.go
@@ -1,7 +1,6 @@
 package notes
 
 import (
-	"encoding/json"
 	"net/http"
 
 	repository_interfaces "github.com/Scowluga/Notera/server/repositories/interfaces"
@@ -9,14 +8,38 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// Setup for notes endpoint
-func Setup(router *mux.Router, db *gorm.DB, noteRepository repository_interfaces.NoteRepository) {
-	router.HandleFunc("/boo", BooHandler)
+type NoteSubRouter struct {
+	Router         *mux.Router
+	Db             *gorm.DB
+	noteRepository repository_interfaces.NoteRepository
 }
 
-// BooHandler returns the single JSON result "Boo": "Yes Babe"
-func BooHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	m := map[string]string{"Boo": "Yes Babe"}
-	json.NewEncoder(w).Encode(m)
+// Setup for notes endpoint
+func Setup(router *mux.Router, db *gorm.DB, noteRepository repository_interfaces.NoteRepository) {
+	note := NoteSubRouter{
+		noteRepository: noteRepository,
+	}
+
+	note.Router = router
+
+	note.Db = db
+
+	note.Router.HandleFunc("/notes/media/{mediaID}", note.MediaHandler).Methods("GET")
+}
+
+// MediaHandler handles getting all notes for a specific media
+func (sr *NoteSubRouter) MediaHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	requestedMediaID := vars["mediaID"]
+
+	tx := sr.Db.Begin()
+	notes, err := sr.noteRepository.GetNotesForMedia(tx, requestedMediaID)
+	if err != nil {
+		tx.Rollback()
+
+	}
+
+	// w.WriteHeader(http.StatusOK)
+	// m := map[string]string{"Boo": "Yes Babe"}
+	// json.NewEncoder(w).Encode(m)
 }
